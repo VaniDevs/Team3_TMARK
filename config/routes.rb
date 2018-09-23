b020b8ad5a1647c00ff6f27e7337b7ff8fd6f89c
Rongqing ZHOU <489688547@qq.com>
2018-09-22 20:51:20 -0700

    add UI design

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :shopping_cart_items
-  resources :shopping_carts
-  resources :appointments
-  resources :volunteers
-  resources :clients
-  resources :agencies
-  resources :donors
-  resources :categories 
-  resources :subcategories 
-  resources :products 
-  root to: 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :shopping_cart_items
+    resources :shopping_carts
+    resources :appointments
+    resources :volunteers
+    resources :clients
+    resources :agencies
+    resources :donors
+    resources :categories 
+    resources :subcategories 
+    resources :products 
+    root to: 'products#index'
 end
